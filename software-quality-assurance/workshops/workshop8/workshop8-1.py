729ac7a711b3e7adec09ae3dee18943203d08106
Issa Aljanabi <40276176+IssaAljanabi@users.noreply.github.com>
2023-04-10 17:55:22 -0500

    workshop8

--- software-quality-assurance/workshops/workshop8/workshop8-1.py
+++ software-quality-assurance/workshops/workshop8/workshop8-1.py
@@ -20,10 +20,28 @@ def fuzzyValues():
     #res = divide(2, -1)
     # Negative software testing: divisor check < 0 values
     #res = divide(2, 0)
+    # Negative software testing: divisor check < 0 small float values
+    #res = divide(2, 0.0001)
+    # Negative software testing: divisor check > large float values
+    #res = divide(2, 999999.7771)    
     #Negative software testing: check types: exmaple 1
-    res = divide (2, '1')
+    #res = divide (2, '1')
     #Negative software testing: check types: exmaple 2
-    res = divide ('2', '1')
+    #res = divide ('2', '1')
+    #Negative software testing: check types: exmaple 3 'NULL'
+    #res = divide (2, 'NULL')
+    #Negative software testing: check types: exmaple 4 'NaN'
+    #res = divide (2, 'NaN')
+    #Negative software testing: check types: exmaple 5 '‘-1E+02’as '
+    #res = divide (2, '-1E+02’as')
+    #Negative software testing: check types: exmaple 6 '‘0xffffffffffffffff’as '
+    #res = divide (2, '0xffffffffffffffff')
+    #Negative software testing: check types: exmaple 7 '-9223372036854775808/-1'
+    #res = divide (2, '-9223372036854775808/-1')
+    #Negative software testing: check types: exmaple 8 '<>?:\"{}|_+/-1'
+    #res = divide (2, '<>?:\"{}|_+/-1')
+    #Negative software testing: check types: exmaple 9 '999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999'
+    res = divide (2, '999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999')   
     
     print(res)
     print('='*100)
